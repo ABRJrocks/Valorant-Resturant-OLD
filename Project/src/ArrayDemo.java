d4a309a7e91c0aefb167082fd2e682e9c239a4fb
RJ <abdulrafeh380@gmail.com>
2021-06-06 22:07:13 +0500

    Init

--- Project/src/ArrayDemo.java
+++ Project/src/ArrayDemo.java
@@ -1,23 +1,29 @@
 
 import java.util.Scanner;
 
-public  class ArrayDemo {
-
-    public static void main(String[] args) {
+public class ArrayDemo {
     String[] breakfast = new String[5];
     Scanner in = new Scanner(System.in);
+
+    void setArray() {
         System.out.println("Enter Dishes: ");
-        for (int i = 0; i < breakfast.length; i++)
-        {
+        for (int i = 0; i < breakfast.length; i++) {
             breakfast[i] = in.nextLine();
         }
+    }
+
+    void getArray() {
         System.out.println("Dishes:");
         {
-            for (int i = 0; i < breakfast.length; i++)
-            {
-                System.out.println(breakfast[i]);
+            for (String s : breakfast) {
+                System.out.println(s);
             }
         }
+    }
 
+    public static void main(String[] args) {
+        ArrayDemo a1 = new ArrayDemo();
+        a1.setArray();
+        a1.getArray();
     }
 }
